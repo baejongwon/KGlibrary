5eae8ee82ec49111243725bb8fa19a444d3e1354
jongwon <jongwon@DESKTOP-COV8KMR>
2024-07-02 22:35:59 +0900

    ㅁ

--- library2/src/main/java/com/kg/library/LibraryApplication.java
+++ library2/src/main/java/com/kg/library/LibraryApplication.java
@@ -3,12 +3,13 @@ package com.kg.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 
-public class LibraryApplication {
+public class LibraryApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
