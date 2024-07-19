16855c7856c4d4f13370c1319348049cd63864d2
jongwon <jongwon@DESKTOP-COV8KMR>
2024-07-20 06:41:04 +0900

    redis

--- library2/src/main/java/com/kg/library/LibraryApplication.java
+++ library2/src/main/java/com/kg/library/LibraryApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@EnableRedisHttpSession
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1000) //spring session 기본 30분이나 수정 함
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 
-public class LibraryApplication extends SpringBootServletInitializer{
+public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
